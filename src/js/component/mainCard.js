0524c354dbe4003c5bcaf6d2e94ce2b229fc48e8
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-05 00:00:21 +0000

    Issues rendering components

--- src/js/component/mainCard.js
+++ src/js/component/mainCard.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-
- export default mainCard = (props) =>  
- <div className="card" key={props.id}>
-                       
- <img src={`https://starwars-visualguide.com/assets/img/characters/${props.id}.jpg`} alt={props.name} />
-  <h2>{props.name}</h2>
-  <Link className="btn btn-primary" to={`/single/${props.id}`}>View Details</Link>
-</div>
+export default mainCard = (props) => (
+  <div className="card" key={props.id}>
+    
+    <img
+      src={`https://starwars-visualguide.com/assets/img/characters/${props.id}.jpg`}
+      alt={props.name}
+    />
+    <h2>{props.name}</h2>
+    <Link className="btn btn-primary" to={`/single/${props.id}`}>
+      View Details
+    </Link>
+  </div>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,17 +4,16 @@ import "../../styles/home.css";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
-    const { store } = useContext(Context);
+  const { store } = useContext(Context);
 
-    return (
-        <div className="text-center mt-5">
-            start
-            {store.characters && store.characters.map(el => 
-            <MainCard
-            id={el.uid}
-            name={el.name} >   
-              </MainCard>  )}
-        </div>
-    );
+  return (
+    <div className="text-center mt-5">
+      start
+      <input className="input"></input>
+      {store.characters &&
+        store.characters.map((el) => (
+          <MainCard id={el.uid} name={el.name}></MainCard>
+        ))}
+    </div>
+  );
 };
-
