23a140ee9aa96db20e5f231abddffc361695a8d3
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-05 12:11:21 +0000

    SIngle.js is working

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,59 +1,60 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-	  store: {
-		characters: [],
-	  },
-	  actions: {
-		// Use getActions to call a function within a fuction
-		exampleFunction: () => {
-		  getActions().changeColor(0, "green");
-		},
-  
-		getCharacters: async () => {
-		  try {
-			const characterRes = await fetch("https://www.swapi.tech/api/people");
-  
-			const charactersData = await characterRes.json();
-  
-			setStore({ characters: [...charactersData.results] });
-		  } catch (error) {
-			console.log("error fetching characters", error);
-		  }
-		},
-  
-		getOneCharacters: async (id) => {
-		  try {
-			const characterRes = await fetch(
-			  "https://www.swapi.tech/api/people/" + id
-			);
-			const charactersData = characterRes.json();
-			setStore({ SingleCharacters: charactersData.result });
-		  } catch (error) {
-			console.log("error fetching characters", error);
-		  }
-		},
-		loadSomeData: () => {
-		  /**
-					  fetch().then().then(data => setStore({ "foo": data.bar }))
-				  */
-		},
-		changeColor: (index, color) => {
-		  //get the store
-		  const store = getStore();
-  
-		  //we have to loop the entire demo array to look for the respective index
-		  //and change its color
-		  const demo = store.demo.map((elm, i) => {
-			if (i === index) elm.background = color;
-			return elm;
-		  });
-  
-		  //reset the global store
-		  setStore({ demo: demo });
-		},
-	  },
-	};
+  return {
+    store: {
+      characters: [],
+      singleCharacter: null,
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+
+      getCharacters: async () => {
+        try {
+          const characterRes = await fetch("https://www.swapi.tech/api/people");
+
+          const charactersData = await characterRes.json();
+
+          setStore({ characters: [...charactersData.results] });
+        } catch (error) {
+          console.log("error fetching characters", error);
+        }
+      },
+
+      getOneCharacters: async (id) => {
+        try {
+          const characterRes = await fetch(
+            "https://www.swapi.tech/api/people/" + id
+          );
+          const charactersData = await characterRes.json();
+
+          setStore({ singleCharacter: charactersData.result });
+        } catch (error) {
+          console.log("error fetching characters", error);
+        }
+      },
+      loadSomeData: () => {
+        /**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
+
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
+
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
   };
-  
-  export default getState;
-  
+};
+
+export default getState;
--- src/js/views/single.js
+++ src/js/views/single.js
@@ -3,24 +3,24 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
+export const Single = (props) => {
+  const { store, actions } = useContext(Context);
+  const { theid } = useParams();
+  console.log(theid); // corrected
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const {theid} = useParams();
-	console.log(params)
+  useEffect(() => {
+    actions.getOneCharacters(theid);
+  }, []); // dependency array added
 
-	useEffect = (() => {
-		actions.getOneCharacters(theid)
-	}, [])
-	return (
-		<div className="container">
-			<div className="card">
-				<p>Name:{store.singleCharacter?.properties?.name}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="card">
+        <p>Name:{store.singleCharacter?.properties?.name}</p>
+      </div>
+    </div>
+  );
 };
 
 Single.propTypes = {
-	match: PropTypes.object
+  match: PropTypes.object,
 };
