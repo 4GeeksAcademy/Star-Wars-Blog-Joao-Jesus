0cfceae8e6e9ad96c91c07348dd3b152f95449eb
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-04-15 23:17:14 +0000

    added star wars lgo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-dark mb-3 ">
-			<Link to="/" className="btn btn-primary m-3">
-				<span className="navbar-brand text-light h1">Home page</span>
-			</Link>
-			<div className="ml-3">
-				<Link to="/demo">
-				<Link className="btn btn-primary m-3" to="/favorites">View Favorites</Link>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-light bg-dark mb-3 ">
+            <Link to="/" className="btn btn-danger m-3">
+                <span className="navbar-brand text-light h1">Home page</span>
+            </Link>
+            <div className="d-flex justify-content-center align-items-center flex-grow-1">
+                <img
+                    className="starWarsLogo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1041px-Star_Wars_Logo.svg.png"
+                    alt="Star Wars Logo"
+                    style={{ width: "200px", height: "auto" }} // Set the width and let the height auto-adjust to maintain aspect ratio
+                />
+            </div>
+            <div className="ml-3">
+                <Link to="/demo">
+                    <Link className="btn btn-danger m-3" to="/favorites">View Favorites</Link>
+                </Link>
+            </div>
+        </nav>
+    );
 };
